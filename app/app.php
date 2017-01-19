6eb3ef6abc373daa590781c8f244297245df3e5d
Elysia Nason <elysia.avery@gmail.com>
2017-01-19 15:55:33 -0800

    Add further form validation

--- app/app.php
+++ app/app.php
@@ -15,6 +15,12 @@
 
   $app->get("/word_check", function() use($app) {
     $my_WordFrequency = new WordFrequency;
+    $user_sentence = $_GET['sentence'];
+    $user_word = $_GET['word'];
+    if(!isset($user_sentence) || trim($user_sentence) == "" || !isset($user_word) || trim($user_word) == "") {
+      var_dump("You have not entered information in all the fields!");
+      return $app['twig']->render('words.html.twig');
+    }
     $word_count = $my_WordFrequency->checkWordFrequency($_GET['sentence'], $_GET['word']);
     return $app['twig']->render('word_count.html.twig', array('result' => $word_count));
   });
