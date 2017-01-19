174df85f105a186314e540d8fe6dda91720e5a09
Elysia Nason <elysia.avery@gmail.com>
2017-01-19 13:18:39 -0800

    Add third failing test

--- tests/WordFrequencyTest.php
+++ tests/WordFrequencyTest.php
@@ -28,5 +28,18 @@
       //Assert
       $this->assertEquals("she smiled and she laughed" && "she", $result);
     }
+    function test_checkWordFrequency_matchWords()
+    {
+      //Arrange
+      $test_WordFrequency = new WordFrequency;
+      $input_sentence = "She smiled and she laughed.!!!";
+      $input_word = "She";
+
+      //Act
+      $result = $test_WordFrequency->checkWordFrequency($input_sentence, $input_word);
+
+      //Assert
+      $this->assertEquals("she", $result);
+    }
   }
 ?>
